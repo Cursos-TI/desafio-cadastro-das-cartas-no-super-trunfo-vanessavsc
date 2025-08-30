3524fdea460af989f6c0c61f0173f1f174a799f5
vanessa vasconcelos <vasco.vanessa21@gmail.com>
2025-08-30 20:59:33 +0000

    crieisupertrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+int main (){
+    printf("Desafio Super trunfo - Países!\n");
+
+
+
+
+
+
+
+
+
+}
+
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
